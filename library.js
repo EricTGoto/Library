ec83821e09635ef9bd687de6742333f28c25ecc9
EricTGoto <erictomokigoto@gmail.com>
2022-04-20 21:05:29 -0700

    added functionality to delete books

--- library.js
+++ library.js
@@ -4,7 +4,7 @@ function Book(title, author, read=false) {
     this.title = title;
     this.author = author;
     this.read = read;
-    this.bookNumber = Math.random() * 10000;
+    this.bookNumber =  Math.floor(Math.random() * 10000);
 }
 
 function addBookToLibrary(e) {
@@ -20,23 +20,28 @@ function displayBook(book) {
     // create book "cards" for every book in myLibrary
     const bookCard = document.createElement("div");
     bookCard.classList.add("bookCard");
-    
+    bookCard.classList.add(book.bookNumber)
     const bookCardTitle = document.createElement("div");
     bookCardTitle.textContent = `Title: ${book.title}`;
     const bookCardAuthor = document.createElement("div");
     bookCardAuthor.textContent = `Author: ${book.author}`
     const bookCardDeleteButton = document.createElement("button")
     bookCardDeleteButton.textContent = "X";
-    
+    bookCardDeleteButton.addEventListener("click", deleteBook)
     bookCard.appendChild(bookCardTitle);
     bookCard.appendChild(bookCardAuthor);
     bookCard.appendChild(bookCardDeleteButton);
     content.appendChild(bookCard);
 }
 
-function deleteBook(book) {
-
+function deleteBook(e) {
+    console.log(e.target.parentElement.className)
+    const cardClass = e.target.parentElement.className.split(" ")[1];
+    const cardToDelete = document.querySelector(`[class="bookCard ${cardClass}"`);
+    myLibrary.filter(book => book.bookNumber == cardClass);
+    cardToDelete.remove();
 }
+
 
 const submitBookButton = document.querySelector(".submitButton")
 const body = document.querySelector("body");
